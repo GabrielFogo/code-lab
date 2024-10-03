ee863f61b635920dea15895e8341ec2743626e37
Pedro <vasconcellospedro213@gmail.com>
2024-10-03 17:55:19 -0300

    Iniciando tela de Editar Perguntas

---
+++ Controllers/PerguntasController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeLab.Controllers
+{
+    public class PerguntasController : Controller
+    {
+        [HttpGet]
+        [Route("/Editar")]
+        public IActionResult Index()
+        {
+            return View("Editar");
+        }
+    }
+}
