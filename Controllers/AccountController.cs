4335c4ee215ac3f95f0f3ad69bf8a837b79e322c
GabrielFogo <gabrielcostal164@gmail.com>
2024-10-03 18:00:57 -0300

    iniciando a tela de login

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeLab.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        [HttpGet]
+        [Route("/Login")]
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+    }
+}
