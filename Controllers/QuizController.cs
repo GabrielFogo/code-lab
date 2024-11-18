b4bd1811c6bc35ba8c947e89d13a6c5d42e78bde
GabrielFogo <gabrielcostal164@gmail.com>
2024-11-18 10:12:39 -0300

    feat: atualiza o quiz ao responder

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -1,27 +1,45 @@
-﻿using System.Text.Json;
+﻿using System.Security.Claims;
+using System.Text.Json;
 using CodeLab.Models;
+using CodeLab.Repositories;
 using CodeLab.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CodeLab.Controllers;
 
+[Authorize]
 public class QuizController : Controller
 {
     private readonly IPerguntaRepository _perguntaRepository;
+    private readonly IQuizRepository _quizRepository;
 
-    public QuizController(IPerguntaRepository perguntaRepository)
+    public QuizController(IPerguntaRepository perguntaRepository, IQuizRepository quizRepository)
     {
         _perguntaRepository = perguntaRepository;
+        _quizRepository = quizRepository;
     }
-    
-    public async Task<IActionResult> Index([FromQuery] string nivel = "1", string lang = "html", int page = 1)
+
+    public async Task<IActionResult> Index([FromQuery] string nivel = "1", string lang = "html", int page = 1, string quizId = "")
     {
         var pergunta = await _perguntaRepository.GetPaginatedAsync(lang, nivel, page, 1);
-        
+
         if (pergunta.Count <= 0)
         {
             return View("QuizConcluido");
+        }
+
+        if (string.IsNullOrEmpty(quizId)) {
+            var quiz = new Quiz()
+            {
+                UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),
+                Nivel = nivel,
+                Linguagem = lang,
+            };
+
+            var quizCriado = await _quizRepository.CreateAsync(quiz);
+            quizId = quizCriado.Id!;
         }
         
         TempData["Pergunta"] =  JsonSerializer.Serialize(pergunta[0]);
@@ -29,7 +47,8 @@ public class QuizController : Controller
         var viewModel = new QuizViewModel()
         {
             PerguntaDescricao = pergunta[0].Description,
-            Alternativas = pergunta[0].Alternativas
+            Alternativas = pergunta[0].Alternativas,
+            QuizId = quizId
         };
         
         ViewData["Page"] = page;
@@ -39,21 +58,27 @@ public class QuizController : Controller
         return View(viewModel);
     }
 
-    public async Task<IActionResult> Responder([Bind("AlternativaSeleciona", "QuizId")] QuizViewModel model, int page, string lang, string nivel)
+    public async Task<IActionResult> Responder([Bind("AlternativaSeleciona", "QuizId")] QuizViewModel model, int page, string lang, string nivel,string quizId)
     {
         if (!ModelState.IsValid)
-            return View("Index", model);
-    
+            return RedirectToAction("Index", new { lang, nivel, page = page, quizId });
+
         var perguntaJson = TempData["Pergunta"] as string;
         var pergunta = JsonSerializer.Deserialize<Pergunta>(perguntaJson!);
+        var quiz = await _quizRepository.GetQuizsAsync(quizId);
     
         if (model.AlternativaSeleciona != pergunta!.AlternativaCorreta)
         {
-            pergunta.NumeroDeErros += 1;
-            await _perguntaRepository.UpdateAsync(pergunta.Id!, pergunta);
+            quiz.PerguntasErradas.Add(pergunta.Id);
+        }
+        else
+        {
+            quiz.PerguntasAcertadas.Add(pergunta.Id);
         }
         
-        return RedirectToAction("Index", new { lang, nivel, page = page + 1 });
+        await _quizRepository.UpdateAsync(quizId, quiz);
+        
+        return RedirectToAction("Index", new { lang, nivel, page = page + 1, quizId });
     }
 
 }
---
+++ Models/Quiz.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.Numerics;
+
+namespace CodeLab.Models
+{
+    public class Quiz
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string? Linguagem { get; set; }
+        public string? UserId { get; set; }
+        public List<string> PerguntasAcertadas { get; set; } = new List<string>();
+        public List<string> PerguntasErradas { get; set; } = new List<string>();
+
+        public int NumeroDeAcertos => PerguntasAcertadas.Count;
+        public int NumeroDeErros => PerguntasErradas.Count;
+
+        public string? Nivel { get; set; }
+    }
+}
